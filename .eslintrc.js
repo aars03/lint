0dc75a2178dda7dadefbd003af2c5c678df601dd
John Doe <shindeaarti00001@gmail.com>
2024-04-12 12:08:59 +0530

    updated workflows

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
-    root: true,
-    env: {
-      node: true,
-      es6: true,
-    },
-    extends: ['eslint:recommended', 'plugin:node/recommended'],
-    rules: {
-      // Add your custom rules here
-    },
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020, // Target ECMAScript 2020 (ES11)
+  },
+  extends: ['eslint:recommended', 'plugin:node/recommended'],
+  rules: {
+    // Add your custom rules here
+  },
 };
+
