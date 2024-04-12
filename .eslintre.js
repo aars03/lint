882acceb3c05065faf4ab3ce9f894e529fdf173f
John Doe <shindeaarti00001@gmail.com>
2024-04-12 12:01:38 +0530

    updated workflows

---
+++ .eslintre.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+      es6: true,
+    },
+    extends: ['eslint:recommended', 'plugin:node/recommended'],
+    rules: {
+      // Add your custom rules here
+    },
+  };
